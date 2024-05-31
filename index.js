3aa8e3f80c382d7cab13efb9c7cd7d4111ac61e0
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-01 01:20:41 +0600

    ordered item

--- index.js
+++ index.js
@@ -98,8 +98,7 @@ const paymentSchema = new mongoose.Schema({
     email:  { type: String, },
     price:  { type: String, },
     date:  { type: Date, },
-    cartId:  { type: Array, },
-    productId:  { type: Array, },
+    orderedItems: { type: Object, },
     status:  { type: String, },
     transactionId:  { type: String, },
 })
